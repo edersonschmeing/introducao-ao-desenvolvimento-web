befb08cf83e070d8ac823b6441b4b414e1b8c4f6
edersonschmeing <edersonschmeing@gmail.com>
2023-10-01 22:21:52 -0300

    js básico

--- javascript/novos-exemplos/01-basico/01-hello-world.js
+++ javascript/novos-exemplos/01-basico/01-hello-world.js
@@ -14,6 +14,9 @@ https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Grammar_and_types
 // /* Você não pode, porém, /* aninhar comentários */ SyntaxError */
 
 
+// Sintaxe strict mode para todo o script
+"use strict";
+
 function saida(t) {
   document.write('<p>' + t + '</p>');
 }
