2e0d4be0b6cff8751d13a9b63c680a28e1620be7
edersonschmeing <edersonschmeing@gmail.com>
2023-10-07 18:19:57 -0300

    nomes de pastas

--- 2023-2/logica-de-programacao-com-java-script/cap12/data/db_config.js
+++
@@ -1,4 +0,0 @@
-const knex = require('knex')
-const config = require("../knexfile.js")
-const dbKnex = knex(config.development)
-module.exports = dbKnex
--- 2023-2/logica-de-programacao-com-java-script/cap13/src/config_axios.js
+++
@@ -1,3 +0,0 @@
-import axios from 'axios';
-// cria uma instance Axios com a URL base do Web Service a ser acessado pelo app
-export const inAxios = axios.create({baseURL: 'http://localhost:3001/'});
