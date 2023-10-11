0930ad691cee84c7dc696fa73e5ff1280a4bdb18
edersonschmeing <edersonschmeing@gmail.com>
2023-10-10 22:19:36 -0300

    arraus

--- 2023-2/javascript-exemplos-aula/08-arrays.js
+++ 2023-2/javascript-exemplos-aula/08-arrays.js
@@ -8,7 +8,6 @@ const arr2 = Array(elemento0, elemento1, ..., elementoN);
 const arr3 = [elemento0, elemento1, ..., elementoN];
 
 */
-
 const array01 = []
 const array02 = Array()
 const array03 = new Array()
@@ -16,7 +15,6 @@ const array03 = new Array()
 //var arr = Array(15); // 15 é o tamanho do array
 //var arr = [];
 //arr.length = 12; // 12 tamanho do vetor
-
 
 console.log("\ntypeof da variável")
 console.log('typeof array01:', typeof array01); // array
@@ -44,7 +42,7 @@ console.log(array2d[2]) //3
 
 //inicializar um array 
 console.log("\ninicializa array com tamanho e valor padrão")
-array06 = Array(12).fill(0)
+let array06 = Array(12).fill(0)
 console.log(array06)
 
 //tamanho do array
@@ -68,7 +66,6 @@ console.log("\nadicionar dois itens no começo de um array")
 array07.unshift(-2, -1)
 console.log(array07)
 
-
 //remover um item a um array
 console.log("\nremover um item a um array")
 array07.pop(array07)
@@ -90,3 +87,31 @@ const array10 = [15,16]
 const array11 = [...array10, ...array09]
 console.log(array11)
 
+
+console.log("\ninteração em arrays")
+var cores = ["vermelho", "verde", "azul"];
+for (var i = 0; i < cores.length; i++) {
+  console.log(cores[i]);
+}
+
+cores.forEach(function (cor) {
+  console.log(cor);
+});
+
+//ES6 Arrow Functions(funções seta)
+cores.forEach((cor) => console.log(cor));
+
+console.log("\nordenção de arrays")
+console.log(cores.sort)
+
+// inicia no índice 1 e extrai todos os elementos
+var meu_array = new Array("a", "b", "c", "d", "e");
+meu_array = meu_array.slice(1, 4); 
+// até o índice 3, retornado [ "b", "c", "d"]
+console.log(meu_array)
+
+/*
+array11.find((15, indice, array) => {
+  console.log(indice)
+})
+*/
