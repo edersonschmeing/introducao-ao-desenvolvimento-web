3badf5be7d06185086c4701533eabb0bb918d15e
edersonschmeing <edersonschmeing@gmail.com>
2023-10-14 16:58:15 -0300

    exemplos

--- 2023-2/javascript-exemplos-aula/16-JSON.js
+++ 2023-2/javascript-exemplos-aula/16-JSON.js
@@ -1,99 +1,32 @@
 /*
-https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Classes
+O que é JSON?
+JSON significa notação de objeto JavaScript
+JSON é um formato leve de troca de dados
+JSON é texto simples escrito em notação de objeto JavaScript
+JSON é usado para enviar dados entre computadores
+JSON é independente de linguagem de progração
 */
 
-console.log("\nClasses");
-class Livro {
-  constructor(titulo, autor, ano, editora) {
-    this.titulo = titulo;
-    this.ano = ano;
-    this.autor = autor;
-    this.editora = editora;
-  }
-  imprime_editora() {
-    console.log(this.editora);
-  }
-}
+//JavaScript possui uma função integrada para converter strings JSON em objetos JavaScript:
+//JSON.parse()
 
-let livro = new Livro("Lógica de Programação com JavaScript", "Edécio Fernando Iepsen" )
-
-console.log(livro); 
-
-console.log("\nsetando valores");
-livro.ano = 2023; 
-livro["editora"] = "Novatec"; 
-
-console.log(livro); 
+//JavaScript também possui uma função integrada para converter um objeto em uma string JSON:
+//JSON.stringify()
 
 
-//Herança
-console.log("\nHerança");
-class Livro_TI extends Livro {
-  constructor(titulo, autor, ano, editora, tecnologia) {
-    super(titulo, autor, ano, editora); 
-    this.tecnologia = tecnologia;
-  }
-  imprime_tecnologia() {
-    console.log(this.tecnologia);
-  }
-}
+let json = '{"nome":"Ederson Schmeing", "idade":39, "tipo_sanguineo":null}';
+console.log("\nJSON: ", json);
+objeto = JSON.parse(json);
+console.log("Objeto: ", objeto);
 
-let livro_TI = new Livro_TI(
-  'Programação Web com Node e Express',
-  'Ethan Brown',
-  '2020',
-  'Novatec',
-  'JavaScript'
-);
+obj = {
+         nome: "Isabela do Anjos", 
+         idade : 38, 
+         tipo_sanguino : null
+      };
 
-console.log(livro_TI.titulo);
-livro_TI.imprime_tecnologia();
+console.log("\nObjeto: ", obj);
+let json02 = JSON.stringify(obj)
+console.log("JSON: ", json02);
 
 
-// getter e setters 
-console.log("\ngetter e setters de acesso"); // {4}
-class Pessoa {
-  constructor(nome) {
-    this._nome = nome;
-  }
-  get nome() {
-    return this._nome;
-  }
-  set nome(value) {
-    this._nome = value;
-  }
-}
-let pessoa = new Pessoa('Frodo');
-console.log(pessoa.nome); 
-pessoa.nome = 'Gandalf'; 
-console.log(pessoa.nome);
-pessoa._nome = 'Sam'; 
-console.log(pessoa.nome);
-
-
-
-// usando symbols para atributos private
-console.log("\nusando symbols para atributos private"); // {4}
-let _nome = Symbol();
-class Pessoa2 {
-  constructor(nome) {
-    this[_nome] = nome;
-  }
-
-  get nome() {
-    return this[_nome];
-  }
-
-  set nome(value) {
-    this[_nome] = value;
-  }
-}
-
-let pessoa2 = new Pessoa2('Frodo');
-console.log(pessoa2.nome);
-pessoa2.nome = 'Gandalf';
-console.log(pessoa2.nome);
-pessoa2._nome = 'Sam';
-console.log(pessoa2.nome);
-
-console.log(Object.getOwnPropertySymbols(pessoa2));
