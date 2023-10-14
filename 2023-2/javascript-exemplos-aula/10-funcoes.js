5406c7ee055292318be219f73299dc5cce1f339d
edersonschmeing <edersonschmeing@gmail.com>
2023-10-14 10:07:35 -0300

    exemplos

--- 2023-2/javascript-exemplos-aula/10-funcoes.js
+++ 2023-2/javascript-exemplos-aula/10-funcoes.js
@@ -2,103 +2,106 @@
 escrita para executar uma tarefa específica.*/
 
 // função
-function cumprimento() {
-  console.log('Olá!');
+function cumprimento01() {
+  console.log('\nOlá!');
 }
-cumprimento();
+cumprimento01();
 
 // função com parâmetro/argumento 
-function cumprimento(nome) {
-  console.log("Olá " +  nome);
-  console.log(`Hello, ${nome}!`);
+function cumprimento02(nome) {
+  console.log("\nOlá " +  nome);
+  console.log(`Olá, ${nome}!`);
 }
-cumprimento('olá!');
-cumprimento('olá!', 'Outro texto'); //ignora segundo parâmetro
-cumprimento(); //undefined
+cumprimento02('Ederson');
+cumprimento02('Ederson!', 'Outro texto'); //ignora segundo parâmetro
+cumprimento02(); //undefined
+
+// função com mais parâmetros/argumentos 
+function cumprimento03(nome, tempo) {
+  console.log(`\nOlá, ${nome}!`);
+  console.log(`Tempo esta, ${tempo}!`);
+}
+cumprimento03("Ederson", "chuvoso")
 
 
+//parâmetros default
 
+function cumprimento04(nome = "Ederson") {
+  console.log(`\nOlá, ${nome}!`);
+}
+cumprimento04()
+cumprimento04("Isabela")
+
+function cumprimento05(tempo, nome = "Ederson",) {
+  console.log(`\nOlá, ${nome}!`);
+  console.log(`Tempo esta, ${tempo}!`);
+}
+cumprimento05("chuvoso")
+
+function cumprimento06(name = "Ederson", tempo) {
+  console.log(`Hello, ${name}!`);
+  console.log(`O tempo esta ${tempo} todos os dias.`);
+}
+cumprimento06(undefined, "ensolarado");
+cumprimento06(null, "ensolarado");
 
 
 /* função usando declaração de retorno return */
 function soma(numero1, numero2) {
   return numero1 + numero2;
 }
-
 let resultado = soma(1, 2);
 console.log(resultado);
 
-
-//* ****** EcmaScript 2015 (ES6): arrow functions
-var circleAreaES5 = function circleArea(r) {
-  var PI = 3.14;
-  var area = PI * r * r;
-  return area;
-};
-console.log(circleAreaES5(2));
-
-
-const circleArea = r => { // {1}
-  const PI = 3.14;
-  const area = PI * r * r;
-  return area;
-};
-console.log(circleArea(2));
+//função com dois retorno return
+function verifique_idade(idade) {
+  if (age >= 18) {
+    return "Pode tirar a CNH";
+  }
+  return "Não pode tirar a CNH";
+}
+console.log(verifique_idade(20));
+console.log(verifique_idade(15));
 
 
-const circleArea2 = r => 3.14 * r * r;
-console.log(circleArea2(2));
+//interrompendo a função com return
+function cumprimento07() {
+  console.log("Olá!");
+  return;
+  console.log("Adeus!");
+}
+cumprimento07()
+
+//escopo de váriavel 
+function cumprimento08() {
+  let meu_cumprimento = "Bom dia!";
+}
+cumprimento08();
+console.log(meu_cumprimento);
 
 
-const hello = () => console.log('hello!');
-hello();
+let meu_cumprimento = "Bom dia!";
+function cumprimento09() {
+  console.log(meu_cumprimento);
+}
+cumprimento09(); // Bom dia!
 
+let meu_cumprimento02 = "Bom dia!";
 
-//tratamento de parâmetros
-
-//* ****** EcmaScript 2015 (ES6): Default Parameter Values 
-function sum(x = 1, y = 2, z = 3) {
-  return x + y + z;
+function cumprimento10() {
+  let meu_cumprimento02 = "Boa tarde!";
+  console.log(meu_cumprimento02);
 }
 
-console.log(sum(4, 2)); // outputs 9
+cumprimento10();  // Boa tarde!
+console.log(meu_cumprimento02); // Bom dia!!
 
-// function above is the same as
-function sum2(x, y, z) {
-  if (x === undefined) x = 1;
-  if (y === undefined) y = 2;
-  if (z === undefined) z = 3;
-  return x + y + z;
+//parâmetro rest
+function imprime_parametrosArguments(...parametros){
+  console.log(parametros);
 }
-console.log(sum2(4, 2)); // outputs 9
 
-// or
-function sum3() {
-  var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
-  var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 2;
-  var z = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 3;
-
-  return x + y + z;
-}
-console.log(sum3(4, 2)); // outputs 9
-
-//* ****** EcmaScript 6: spread operator ('...') (https://goo.gl/8equk5)
-let params = [3, 4, 5];
-console.log(sum(...params)); // ES2015
-console.log(sum.apply(undefined, params)); // ES5
-
-let numbers = [1, 2, ...params]; // pushing values into array
-console.log(numbers);
-
-//* ****** EcmaScript 6: rest parameter ('...') (https://goo.gl/LaJZqU)
-function restParamaterFunction(x, y, ...a) {
-  return (x + y) * a.length;
-}
-console.log(restParamaterFunction(1, 2, 'hello', true, 7)); // outputs 9;
-
-// code above is the same as ES5:
-function restParamaterFunction2(x, y) {
-  var a = Array.prototype.slice.call(arguments, 2);
-  return (x + y) * a.length;
-}
-console.log(restParamaterFunction2(1, 2, 'hello', true, 7));
+imprime_parametros("A", "B", "C"); 
+// [ 'A', 'B', 'C' ]
+imprime_parametros(1, 2, 3, 4, 5);
+// [ 1, 2, 3, 4, 5 ]
